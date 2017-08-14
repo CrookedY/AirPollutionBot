ccb70dc2366a4e1f6d0ee54423a9ee678d9ca77b
CrookedY <hannah_sewell1992@hotmail.com>
2017-08-14 15:03:09 +0100

    Revert "Add pollutant (#4)" (#5)
    This reverts commit 922f5bda2572c12724eff05cf8394b90e7b6994e.

--- airpoldata.py
+++ airpoldata.py
@@ -1,53 +1,46 @@
 from urllib2 import Request, urlopen, URLError
 import json, os
-from getdata import NOXresult, p2result
 
-def whichresult(pollutant):
-	if pollutant[-1][u'value'] !=-99.0:
+#this script accesses the data and selects the appropriate values
+
+request = Request('https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/455/getData')
+
+try:
+	response = urlopen(request)
+	data = response.read()
+except URLError, e:
+    print 'error:', e
+
+result= json.loads (data)
+
+result2 = result [u'values']
+
+def whichresult():
+	if result2[-1][u'value'] !=-99.0:
 	#	print result2[-1][u'value']
-		return pollutant[-1][u'value']
+		return result2[-1][u'value']
 	else:
-		if pollutant[-2][u'value']!=-99.0:
+		if result2[-2][u'value']!=-99.0:
 			#print result2[-2][u'value']
-			return pollutant[-2][u'value']
+			return result2[-2][u'value']
 		else:
-			if pollutant[-3][u'value']!=-99.0:
+			if result2[-3][u'value']!=-99.0:
 				#print result2[-3][u'value']
-				return pollutant[-3][u'value']
-			elif pollutant[-4][u'value']!=-99.0:
+				return result2[-3][u'value']
+			elif result2[-4][u'value']!=-99.0:
 				#print result2[-3][u'value']
-				return pollutant[-4][u'value']
-NOX = whichresult(NOXresult)
-P2 = whichresult(p2result)
-
-#print NOX
+				return result2[-4][u'value']
 
 if os.environ.get("DEBUG"):
   print whichresult()
 
-def highorlowp2():
-	if P2<=35:
+def highorlow():
+	if whichresult()<=35:
 		return 'low'
-	elif P2>=54:
+	elif whichresult()>=54:
 		return 'high'
 	else:
 		return 'medium'
 
 if os.environ.get("DEBUG"):
 	print highorlow()
-
-def highorlowNOX():
-	if NOX<=200:
-		return 'low'
-	elif NOX>=400:
-		return 'high'
-	else:
-		return 'medium'
-
-def highorlow():
-	if highorlowNOX() =='high' or highorlowp2() == 'high':
-		return 'high'
-	elif highorlowNOX =='medium' or highorlowp2 =='medium':
-		return 'medium'
-	else:
-		return 'low'

a026bf32a4cfddf4f7078218b5ec51aba63dbf65
CrookedY <hannah_sewell1992@hotmail.com>
2017-09-05 10:23:52 +0100

    Change tweet (#7)
    * change tweet text
    
    * WIP loop to get data for station number

--- liststations.py
+++ liststations.py
@@ -10,9 +10,36 @@ except URLError, e:
     print 'error:', e
 
 stations= json.loads (data)
+#extract out station 2
+stations2 = stations [7]
 
-#print stations
+properties = stations2[u'properties']
+#extract ID so can be use in link
+ID = properties[u'id']
+#print ID
+url = ('https://uk-air.defra.gov.uk/sos-ukair/api/v1/stations/'+str(ID))
 
-stations2 = stations [1]
+request2 = Request (url)
+try:
+	response = urlopen(request2)
+	data2 = response.read()
+except URLError, e:
+    print 'error:', e
 
-print stations2
+#contains  station properties data. Need to get to timecourse ID
+station_prop = data2
+station_prop_json= json.loads (station_prop)
+#ID is a key in dictionary so need to extract as a key
+a= station_prop_json[u'properties'][u'timeseries'].keys()
+i=a[0]
+
+url2 =('https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/'+str(i) +'/getData')
+
+request3 = Request(url2)
+try:
+	response = urlopen(request3)
+	data3 = response.read()
+except URLError, e:
+    print 'error:', e
+
+print data3
--- twitterbot.py
+++ twitterbot.py
@@ -1,16 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import tweepy
 import os
 from airpoldata import NOX, P2, highorlow
 
-#this script runs the twitterbot
+this script runs the twitterbot
 auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
 
-airnow = ('The air pollution now is '+ (highorlow()) + '. Particulate matter <2.5 um = ' + str(P2)+ 'ug/m3, Nitrous oxides concentration = ' + str(NOX) + 'ug/m3 #airpollution')
+
+
+
+airnow = ('The #airpollution now is '+ (highorlow()) + '. Particulate matter <2.5 um = ' + str(P2)+ u'µg/m³, Nitrous oxides concentration = ' + str(NOX) + u'µg/m³ #airpollution')
 if os.environ.get("DEBUG"):
   print (airnow)
 
-print airnow
 
-api.update_status(status=airnow)
+
+if P2 != None and NOX != None:
+    pi.update_status(status=airnow)

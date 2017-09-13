6a83d67ad837ce702d94be8e0ffa18b01d9daa20
Hannah Sewell <hannah_sewell1992@hotmail.com>
2017-09-13 12:11:32 +0100

    add missing #

--- twitterbot.py
+++ twitterbot.py
@@ -4,7 +4,7 @@ import tweepy
 import os
 from airpoldata import NOX, P2, highorlow
 
-this script runs the twitterbot
+#this script runs the twitterbot
 auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)

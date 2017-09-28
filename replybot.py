2e1c02e696b81b12a8d9d1005db8369ba67e24fc
Hannah Sewell <hannah_sewell1992@hotmail.com>
2017-09-28 14:36:57 +0100

    loop through tweets and get data

--- replybot.py
+++ replybot.py
@@ -13,6 +13,13 @@ import json
 
 geolocator = Nominatim()
 
+def getValidTimeseriesKey(timerseries_keys, offering_id):
+	invalid_offering = '9999999999'
+	if offering_id == invalid_offering:
+		return timeseries_keys[1]
+	else:
+		return timeseries_keys[0]
+
 
 auth = tweepy.OAuthHandler('', '')
 auth.set_access_token('', '')
@@ -26,105 +33,82 @@ searchstrings = ["@shefairpol",
                   ]
 tweetslookup = api.search(q="Sheffield Air Pollution")
 
-print (tweetslookup[0].text)
-
-Place = GeoText(tweetslookup[1].text)
-PlaceNames = Place.cities
-PlaceName = Place.cities[0]
-location = str(PlaceName.translate(string.punctuation)) #needs to be a forloop?
-
-print (PlaceNames)
-
-###############################################################################
-
-tweetedLocation = geolocator.geocode(location, timeout = None)
-
-LocationCoordinates = (tweetedLocation.longitude,tweetedLocation.latitude)
-
-print (LocationCoordinates)
+# print(tweetslookup[1].text.encode('utf-8','ignore'))
+#
+# print(tweetslookup[1].user.screen_name)
 
 ListofStationCoordinates = finallist.loc[ : , "latandlong"]
 
-#print (ListofStationCoordinates)
+for i in tweetslookup:
+    reply_to = i.user.screen_name
+    place = GeoText(i.text)
+    PlaceName = place.cities
+    if len(PlaceName)==1:
+        location = json.dumps(PlaceName).translate(None, string.punctuation)
+        tweetedLocation = geolocator.geocode(location, timeout = None)
+        LocationCoordinates = (tweetedLocation.longitude,tweetedLocation.latitude)
+        shortest_distance = None
+        shortest_distance_coordinates = None
+        for station in ListofStationCoordinates:
+            distance = vincenty(LocationCoordinates, station)
+            if distance < shortest_distance or shortest_distance is None:
+                shortest_distance = distance
+                shortest_distance_coordinates = station
+                #print (shortest_distance_coordinates)
+                ClosestStation = finallist[finallist.latandlong.isin([shortest_distance_coordinates])]
+                #print(ClosestStation)
+                ClosestStationKeys = (ClosestStation.loc[:, "ID"])
+                #print (ClosestStationKeys)
+                ####################################################################################
+                requestpoll = Request ('http://dd.eionet.europa.eu/vocabulary/aq/pollutant/json')
+                try:
+                    response = urlopen(requestpoll)
+                    pollutant_prop = response.read()
+                except URLError, e:
+                    print 'error:', e
 
+                json_pollutantlist = json.loads(pollutant_prop)
+                jsonpollutantlistdictionaries = json_pollutantlist[u'concepts']
+                #######################################################################
 
-shortest_distance = None
-shortest_distance_coordinates = None
+                listofpollutants = {}
+                for pollutant in jsonpollutantlistdictionaries:
+                    statID = pollutant['@id']
+                    pollutantname = pollutant[u'prefLabel'][0]['@value']
+                    listofpollutants.update ({statID:pollutantname})
 
-for station in ListofStationCoordinates:
-    distance = vincenty(LocationCoordinates, station)
-    if distance < shortest_distance or shortest_distance is None:
-        shortest_distance = distance
-        shortest_distance_coordinates = station
+                ###############################################################
+                ID=(ClosestStationKeys)
+                listofstationsdata = []
+#
+                for i in ID:
+                    url = ('https://uk-air.defra.gov.uk/sos-ukair/api/v1/stations/'+str(i))
+                    request2 = Request (url)
+                    try:
+                    	response = urlopen(request2)
+                    	station_data = response.read()
+                    except URLError, e:
+                        print 'error:', e
 
-print (shortest_distance_coordinates)
+                    station_prop_json = json.loads (station_data)
+                    station_time_series = station_prop_json[u'properties'][u'timeseries']
+                    timeseries_keys = (station_time_series.keys())
+                    first_timeseries = station_time_series[timeseries_keys[0]]
+                    offering_id = first_timeseries[u'offering'][u'id']
+                    first_timeserieskey = getValidTimeseriesKey(timeseries_keys, offering_id)
+                    station_pollutant = first_timeseries[u'category'][u'id']
+                    station_ID = first_timeseries[u'feature'][u'id']
+                    StationName = PlaceName
+                    PollutantName = listofpollutants.get(station_pollutant)
+                    url2getdata = ('https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/'+str(first_timeserieskey) +'/getData')
 
-ClosestStation = finallist[finallist.latandlong.isin([shortest_distance_coordinates])]
+                    request_time_series_data = Request(url2getdata)
+                    try:
+                    	response = urlopen(request_time_series_data)
+                    	time_series_data = response.read()
+                    except URLError, e:
+                        print 'error:', e
 
-#print(ClosestStation)
+                    listofstationsdata.append((StationName, PollutantName, time_series_data))
 
-ClosestStationKeys = ClosestStation.loc[:, "ID"]
-
-print(ClosestStationKeys)
-
-##########################################################################
-
-def getValidTimeseriesKey(timerseries_keys, offering_id):
-	invalid_offering = '9999999999'
-	if offering_id == invalid_offering:
-		return timeseries_keys[1]
-	else:
-		return timeseries_keys[0]
-
-requestpoll = Request ('http://dd.eionet.europa.eu/vocabulary/aq/pollutant/json')
-try:
-    response = urlopen(requestpoll)
-    pollutant_prop = response.read()
-except URLError, e:
-    print 'error:', e
-
-json_pollutantlist = json.loads(pollutant_prop)
-jsonpollutantlistdictionaries = json_pollutantlist[u'concepts']
-
-listofpollutants = {}
-for pollutant in jsonpollutantlistdictionaries:
-    statID = pollutant['@id']
-    pollutantname = pollutant[u'prefLabel'][0]['@value']
-    listofpollutants.update ({statID:pollutantname})
-
-
-ID=(ClosestStationKeys)
-
-listofstationsdata = []
-
-for i in ID:
-    url = ('https://uk-air.defra.gov.uk/sos-ukair/api/v1/stations/'+str(i))
-    request2 = Request (url)
-    try:
-    	response = urlopen(request2)
-    	station_data = response.read()
-    except URLError, e:
-        print 'error:', e
-
-    station_prop_json = json.loads (station_data)
-    station_time_series = station_prop_json[u'properties'][u'timeseries']
-    timeseries_keys = (station_time_series.keys())
-    first_timeseries = station_time_series[timeseries_keys[0]]
-    offering_id = first_timeseries[u'offering'][u'id']
-    first_timeserieskey = getValidTimeseriesKey(timeseries_keys, offering_id)
-    station_pollutant = first_timeseries[u'category'][u'id']
-    station_ID = first_timeseries[u'feature'][u'id']
-    StationName = PlaceName
-    PollutantName = listofpollutants.get(station_pollutant)
-    url2getdata = ('https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/'+str(first_timeserieskey) +'/getData')
-
-    request_time_series_data = Request(url2getdata)
-    try:
-    	response = urlopen(request_time_series_data)
-    	time_series_data = response.read()
-    except URLError, e:
-        print 'error:', e
-
-    listofstationsdata.append((StationName, PollutantName, time_series_data))
-
-print listofstationsdata
+                #reply_status = ('@'+'
